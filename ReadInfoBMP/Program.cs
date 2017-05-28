8538ca5a86db10381210ca8f4be0e9106a715eca
Cesar Morales <cmorales@thedataage.com>
2017-05-27 20:27:13 -0600

    Aplicacion de consola que lee información de archivo binario .BMP

---
+++ ReadInfoBMP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadInfoBMP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileStream = File.Open("./Files/Image.bmp", FileMode.OpenOrCreate);
+            var binReader = new BinaryReader(fileStream);
+
+            binReader.BaseStream.Seek(18, SeekOrigin.Begin);
+            int width = binReader.ReadInt32();
+
+            binReader.BaseStream.Seek(22, SeekOrigin.Begin);
+            int height = binReader.ReadInt32();
+
+            binReader.BaseStream.Seek(28, SeekOrigin.Begin);
+            short bpp = binReader.ReadInt16();
+
+            Console.WriteLine($"Ancho = {width}");
+            Console.WriteLine($"Alto = {height}");
+            Console.WriteLine($"Bit por pixel= {bpp}");
+
+            binReader.Close();
+            fileStream.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
